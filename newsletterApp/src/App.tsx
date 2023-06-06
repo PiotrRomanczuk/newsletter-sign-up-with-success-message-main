568540f0a78ca10d327ca5b14f7ed985e1b0a891
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-06-06 11:44:58 +0200

    first commit

---
+++ newsletterApp/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<p>Stay updated!</p>
+			<p>Email address email@company.com Subscribe to monthly newsletter</p>
+			<p>
+				Product discovery and building what matters Measuring to ensure updates
+				are a success And much more!
+			</p>
+			<p> Join 60,000+ product managers receiving monthly updates on:</p>
+			<p></p>
+		</div>
+	);
+}
+
+export default App;
